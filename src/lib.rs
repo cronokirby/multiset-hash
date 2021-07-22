3f20aa6a760a0d5e415e17358a1a0475a592b722
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-22 19:51:59 +0200

    Define RistrettoHash struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[derive(Clone, Default)]
+struct RistrettoHash {}
+
+impl RistrettoHash {
+    pub fn add(data: impl AsRef<u8>, multiplicity: u64) {
+        unimplemented!()
     }
 }
